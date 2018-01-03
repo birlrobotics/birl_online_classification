99d2231c886208489287f1f7fbd5033a6a6b4c8d
sklaw <271462243@qq.com>
2018-01-03 09:38:33 +0800

    load model using relative path

--- src/birl_online_classification/anomaly_classifier.py
+++ src/birl_online_classification/anomaly_classifier.py
@@ -1,15 +1,17 @@
 import numpy as np
 from keras.models import load_model
 import pandas as pd
+import os
 
 # if you want to run this code on your computer, pls change this PATH
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def one_hot_decode(encoded_seq):
     return [np.argmax(vector) for vector in encoded_seq]
 
 def run(np_matrix):
-    model = load_model('/home/dennis/mlnd/my_p/anomaly_classcification1/models/model_Anomaly_classification.h5')
+    model = load_model(os.path.join(dir_path, '..', '..', 'models', 'model_Anomaly_classification.h5'))
     '''
     @ np_matrix: 2D numpy array, shape must be (40, 13).
     ''' 
